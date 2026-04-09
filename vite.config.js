e640abc7e24c2779ec8eac97cada83186cc59805
dixon <dixoncarnacete13@gmail.com>
2026-04-09 17:38:56 +0800

    Update base path in vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/olm/",
   plugins: [react()],
-})
-
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          "react-vendor": ["react", "react-dom", "react-router-dom"],
+          "animation-vendor": ["framer-motion"],
+        },
+      },
+    },
+  },
+});
